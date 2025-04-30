7e43f63726e38f3154319399b2f07a63606d99d2
RAIHANA BINTE RAHMAT <23021014@myrp.edu.sg>
2025-04-30 17:37:53 +0800

    Initial commit

---
+++ Football Club Web App/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to serve static files (like styles.css)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home Page', message: 'Welcome to my basic Express app!' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
